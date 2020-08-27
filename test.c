0ffd3eb46d58ab8e4d031157bf69d8ae44311e9e
t1n <t1n@root.t1n>
2020-08-27 18:16:03 +0700

    Sua lan dau

---
+++ test.c
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> slidingWindowMax(vector<int> arr, int k)
+{
+    vector<int> res;
+    if (k > arr.size())
+        return res;
+    int max;
+    for (int i = 0; i <= arr.size() - k; i++)
+    {
+        max = arr[i];
+        for (int j = i + 1; j <= i + k - 1; j++)
+        {
+            max = (arr[j] >= max) ? arr[j] : max;
+        }
+        res.push_back(max);
+    }
+    return res;
+}
